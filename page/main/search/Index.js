cd6f70f2590f0c32ecd6e10c66186493c13f3fbe
SangMin96 <hwon3794@gmail.com>
2021-02-03 01:14:46 +0900

    .

--- page/main/search/Index.js
+++ page/main/search/Index.js
@@ -1,61 +1,56 @@
-import React, {  useCallback, useEffect,  useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
 import styled from 'styled-components/native';
 
 import { useMutation } from '@apollo/react-hooks';
-import {  SEARCH_ROOM } from './Query';
-import { ActivityIndicator,View } from 'react-native';
+import { SEARCH_ROOM } from './Query';
+import { ActivityIndicator, View } from 'react-native';
 import { Input } from '@99xt/first-born';
 import SearchRoom from './SearchRoom';
 
-
-function SearchView({ navigation }) {
-  const [searchData,setSearchData]=useState()
-  const [loading,setLoading]=useState(false)
-  const [searchMt] = useMutation(SEARCH_ROOM)
-
+function SearchView({ navigation, route }) {
+  const [searchData, setSearchData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [searchMt] = useMutation(SEARCH_ROOM);
+  const [value, setValue] = useState();
 
   useEffect(() => {
-    
-  }, []);
+    setValue(route?.params?.text);
+  }, [route, setValue]);
 
- 
-  const onSearchRoom =async (ev)=>{
-    try{
-      setLoading(true)
-      const text =  ev.nativeEvent.text
-      const rslt = await searchMt({ variables: {text} })
-      if(rslt){
-        setSearchData(rslt?.data?.searchRoomList)
-        setLoading(false)
+  const onSearchRoom = async (ev) => {
+    try {
+      setLoading(true);
+      const text = ev.nativeEvent.text;
+      const rslt = await searchMt({ variables: { text } });
+      if (rslt) {
+        setSearchData(rslt?.data?.searchRoomList);
+        setLoading(false);
       }
-    }catch(err){
-
-    }
-  }
+    } catch (err) {}
+  };
 
   return (
     <>
-     {loading ? (
+      {loading ? (
         <View style={{ flex: 1, justifyContent: 'center' }}>
           <ActivityIndicator size="large" color="#0059ff" />
         </View>
       ) : (
         <SearchViewScreen>
-          <Input onSubmitEditing={onSearchRoom} placeholder="방 제목, 닉네임, 태그" />
-          <ScrollView >
+          <Input
+            value={value || ''}
+            onChangeText={(ev) => setValue(ev)}
+            onSubmitEditing={onSearchRoom}
+            placeholder="방 제목, 닉네임, 태그"
+          />
+          <ScrollView>
             {searchData?.map((item) => (
-              <SearchRoom
-                key={item.id}
-                data={item}
-                rDate={item.rDate}
-                uCount={item.uCount}
-                avatar={item.avatar}
-              />
+              <SearchRoom key={item.id} data={item} rDate={item.rDate} uCount={item.uCount} avatar={item.avatar} />
             ))}
           </ScrollView>
         </SearchViewScreen>
-   )}
+      )}
     </>
   );
 }
