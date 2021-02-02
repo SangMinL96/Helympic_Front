17f71258903ec6ecafa1021ac88b04eea976d22d
SangMin96 <hwon3794@gmail.com>
2021-02-03 01:13:51 +0900

    인기 동영상 인덱스

--- page/main/popularity/Index.js
+++ page/main/popularity/Index.js
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
-
+import styled from "styled-components/native"
 import { View, Text, TouchableOpacity } from 'react-native';
+import Popularitys from './Popularitys';
 
 
 
-function Popularity({ navigation }) {
-  useEffect(() => {
-   
-  }, []);
-
+function Popularity() {
+ 
 
   return (
-    <View>
-    
-    </View>
+    <PopularityScreen>
+      <Popularitys />
+    </PopularityScreen>
   );
 }
 
 export default Popularity;
+const PopularityScreen = styled.View`
+ flex: 1;
+  background-color: ${(props) => props.theme.backColor};
+`;
+
